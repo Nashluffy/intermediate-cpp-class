c1062e8542378d3ebed840140c7e30d262e83a52
Nash Luffman <nashluffman@gmail.com>
2019-01-23 08:59:31 -0500

    1/23/19 Class Notes & Output

---
+++ 1_23_18_Notes.cpp
@@ -0,0 +1,10 @@
+/* 
+1/18/19 Notes 
+To compile, g++ -std=c++11 file_name.cpp -o output_file
+
+    Update your local repo from the central repo (git pull upstream master).
+    Make edits, save, git add, and git commit all in your local repo.
+    Push changes from local repo to your fork on github.com (git push origin master)
+    Update the central repo from your fork (Pull Request)
+    Repeat.
+*/
