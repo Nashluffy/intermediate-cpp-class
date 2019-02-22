35a48c1bd376285346d5bc8b8762e59226ce4b9e
Nash Luffman <nashluffman@gmail.com>
2019-02-22 13:11:21 -0500

    2/22/2019 Class Notes & Output

--- 2_22_2019_Notes.cpp
+++ 2_22_2019_Notes.cpp
@@ -1,9 +1,5 @@
-/*Use of dynamic memory to implement dynamic array (like vector)*/
-
-
-
 /* 
-2/20/2019 - Destructors
+2/22/2019 Notes - Copy constructors
 To compile, g++ -std=c++11 file_name.cpp -o output_file
 
     Update your local repo from the central repo (git pull upstream master).
@@ -13,8 +9,8 @@ To compile, g++ -std=c++11 file_name.cpp -o output_file
     Repeat.
 */
 
-using namespace std;
 #include <iostream>
+#include <vector>
 
 class MyVector{
     private:
@@ -24,27 +20,47 @@ class MyVector{
     public:
         ~MyVector(); //Destructor: Cleans stuff up. Here deletes arr
         MyVector() : arr{nullptr} {};
+        MyVector(MyVector& inobj);
         void push(int ele);
-        friend ostream& operator <<(ostream& os, MyVector& v);
+        MyVector& operator=(MyVector& inobj)
 };
-
-MyVector::~MyVector(){
-    delete[] arr;
-    cout << "Destroyed vector" << endl;
-}
-
-ostream& operator <<(ostream& os, MyVector& v){
-    for (int i = 0; i < v.size; i++){
-        os << v.arr[i] << " ";
-    }
-}
 
 int main(){
     MyVector vec;
     vec.push(1);
     vec.push(2);
     vec.push(10);
-//    cout << vec << endl;
+    MyVector vec1(vec); //This requires a copy 
+    vec1.push(42);
+    vec = vec1;
+
+}
+
+MyVector::operator=(MyVector& inobj){
+    delete[] this->arr;
+    this->arr = new int[inobj.cap];   //this-> is a special pointer that points to the current object - here vec1. 
+    for (int i = 0; i < inobj.size; i++){
+        arr[i] = inobj.arr[i];
+    }
+    this->size = inobj.size;
+    this->cap = inobj.cap;
+    return *this;
+}
+
+MyVector::MyVector(MyVector& inobj){    //Copy Constructor
+
+    //This is called shadow copying. DO NOT DO THIS. This only copies the pointers.
+    this->arr = new int[inobj.cap];   //this-> is a special pointer that points to the current object - here vec1. 
+    for (int i = 0; i < inobj.size; i++){
+        arr[i] = inobj.arr[i];
+    }
+    this->size = inobj.size;
+    this->cap = inobj.cap;
+
+}
+
+MyVector::~MyVector(){  //Destructor
+    delete[] arr;
 }
 
 void MyVector::push(int ele){
@@ -68,12 +84,6 @@ void MyVector::push(int ele){
             cap = cap*2;
             arr[size] = ele;
             size++;
-        }
+       }
     }
-    //If yes
-        //Dynamically create array of elements
-        //Insert ele into array
-    //If there is no space available
-        //Double size 
-
 }
